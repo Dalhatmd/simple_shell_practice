bb049f822069429f2c1498c1f2098be67947bc22
dalhatmd <umarwalid4055@gmail.com>
2023-05-13 17:09:12 +0100

    Added a few files

---
+++ main.c
@@ -0,0 +1,13 @@
+#include "shell.h"
+/**
+ * main - Program starts here
+ *
+ * @ac: number or args
+ * @av: array of args
+ *
+ * Return: always(0) success
+ */
+int main(int ac, char *av[])
+{
+	return (0);
+}
