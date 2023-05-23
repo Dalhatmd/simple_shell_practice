919d9616481286f0e6911b410ea975a50525e49e
Ebenezer Woldehana <95696690+Eba-1@users.noreply.github.com>
2023-05-24 01:31:11 +0530

    Betty style fixed for "cleanup.c" file

--- cleanup.c
+++ cleanup.c
@@ -3,13 +3,13 @@
 /**
  * cleanup - Function that free a space dynammically allocated to an array.
  * @argv: Parameter (the array of commands to be freed).
- * 
  * Return: Nothing (it's a viod function).
  */
 
 void cleanup(char **argv)
 {
 	int i;
+	
 	if (argv != NULL)
 	{
 		for (i = 0; argv[i] != NULL; i++)
