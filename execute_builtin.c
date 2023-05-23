22816a08bd5ceceae47e9a3e18d49f280203feea
Ebenezer Woldehana <95696690+Eba-1@users.noreply.github.com>
2023-05-24 01:44:38 +0530

    Betty Style Fixed for "execute_builtin.c" file

--- execute_builtin.c
+++ execute_builtin.c
@@ -1,4 +1,11 @@
 #include "shell.h"
+
+/**
+ * exec_builtin - Function that checks if the command is "exit".
+ *
+ * @argv: Parameter (the command to check).
+ */
+
 void exec_builtin(char **argv)
 {
 	if (strcmp(argv[0], "exit") == 0 && !argv[1])
