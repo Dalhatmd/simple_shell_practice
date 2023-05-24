30a01675cc62fac1db73e8a651c5d01e71dd1712
Ebenezer Woldehana <95696690+Eba-1@users.noreply.github.com>
2023-05-24 23:23:03 +0530

    Update get_loc.c

--- get_loc.c
+++ get_loc.c
@@ -1,52 +1,12 @@
 #include "shell.h"
 
-/*
- * get_location - Function to find the location of the executable file.
- * @command: Parameter (the command to find its executable file).
- *
- * Return: Path of the executable.
- */
-
 char *get_location(char *command)
 {
-	char *path, *path_copy, *path_token, *file_path;
-	int com_len, dir_len;
-	struct stat buffer;
-
-	path = getenv("PATH");
-	if (path)
-	{
-		path_copy = strdup(path);
-		com_len = strlen(command);
-		path_token = strtok(path_copy, ":");
-
-		while (path_token != NULL)
-		{
-			dir_len = strlen(path_token);
-			file_path = malloc(com_len + dir_len);
-			strcpy(file_path, path_token);
-			strcat(file_path, "/");
-			strcat(file_path, command);
-			strcat(file_path, "\0");
-
-			if (stat(file_path, &buffer) == 0)
-			{
-				free(path_copy);
-				return (file_path);
-			}
-			else
-			{
-				free(file_path);
-				path_token = strtok(NULL, ":");
-			}
-		}
-		free(path_copy);
-
-		if (stat(command, &buffer) == 0)
-			return (command);
-		return (NULL);
-
-	}
-
-	return (NULL);
+	char *path, *dir;
+	
+	path = "/bin/";
+	dir = (char *) malloc(1 + strlen(path) + strlen(command));
+	strcpy(dir, path);
+	strcat(dir, command);
+	return (dir);
 }
