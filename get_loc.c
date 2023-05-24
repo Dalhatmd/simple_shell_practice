991f9a14fce11778c2a1e893bc72a2194ebec879
Ebenezer Woldehana <95696690+Eba-1@users.noreply.github.com>
2023-05-24 23:26:39 +0530

    Update get_loc.c

--- get_loc.c
+++ get_loc.c
@@ -1,5 +1,11 @@
 #include "shell.h"
 
+/**
+ * get_location - Function to find path of executable file.
+ * @command: Parameter.
+ * Return: Path of executable file.
+ */
+
 char *get_location(char *command)
 {
 	char *path, *dir;
