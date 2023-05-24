5738269a0f0f501014215eed103f7b9ae4f19ccb
dalhatmd <umarwalid4055@gmail.com>
2023-05-24 23:03:03 +0100

    merging to pull

--- main.c
+++ main.c
@@ -7,35 +7,8 @@
  *
  * Return: 0 for success.
  */
-
-int main(int ac, char *av[])
+int main(void)
 {
-	char *lineptr = NULL;
-	size_t n;
-	ssize_t nchars_read;
-
-	(void)av;
-	(void)ac;
-
-	n = 0;
-
-	while (1)
-	{
-		if (isatty(STDIN_FILENO))
-		{
-			prompt();
-		}
-		nchars_read = getline(&lineptr, &n, stdin);
-
-		if (nchars_read == -1)
-		{
-			printf("Exiting Shell... \n");
-			exit(-1);
-		}
-		lineptr[strcspn(lineptr, "\n")] = '\0';
-
-		input();
-	}
-	free(lineptr);
+	input();
 	return (0);
 }
