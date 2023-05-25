4a19e4c8bf262971c85f1de5826a767d4c5e8272
Dalhatmd <umarwalid4055@gmail.com>
2023-05-25 10:11:37 -0700

    Please work

--- execute.c
+++ execute.c
@@ -4,6 +4,8 @@ void execmd(char **argv)
     char *command = NULL, *actual_command = NULL;
     pid_t pid;
     int status;
+    char **new_argv;
+    int i;
 
     status = 0;
 
@@ -34,11 +36,27 @@ void execmd(char **argv)
                     actual_command = get_location(command);
                     if (actual_command != NULL)
                     {
-                        if (execve(actual_command, argv, NULL) == -1)
+                        int num_tokens = 0;
+                        for (i = 0; argv[i] != NULL; i++)
+                        {
+                            num_tokens++;
+                        }
+
+                        new_argv = malloc(sizeof(char *) * (num_tokens + 1));
+                        new_argv[0] = actual_command;
+                        for (i = 1; argv[i] != NULL; i++)
+                        {
+                            new_argv[i] = argv[i];
+                        }
+                        new_argv[num_tokens] = NULL;
+
+                        if (execve(actual_command, new_argv, NULL) == -1)
                         {
                             perror("execve");
                             exit(EXIT_FAILURE);
                         }
+
+                        free(new_argv);
                     }
                     else
                     {
